3d8e3de0c79f12afb597a7e888cda27246fb64d7
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-03-05 01:29:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// sticky header
+
+window.addEventListener("scroll", () => {
+
+    const header = document.getElementById("header")
+
+    if (window.scrollY > 150) {
+        header.classList.add("sticky")
+    } else {
+        header.classList.remove("sticky")
+    }
+
+})
+
+
+
+// carousel
+
+const track = document.querySelector(".carousel-track")
+const slides = document.querySelectorAll(".carousel img")
+
+let index = 0
+
+document.querySelector(".next").onclick = () => {
+    index++
+    if (index >= slides.length) index = 0
+    track.style.transform = `translateX(-${index * 100}%)`
+}
+
+document.querySelector(".prev").onclick = () => {
+    index--
+    if (index < 0) index = slides.length - 1
+    track.style.transform = `translateX(-${index * 100}%)`
+}
+
+
+
+// faq
+
+document.querySelectorAll(".faq-question").forEach(btn => {
+
+    btn.onclick = () => {
+        const ans = btn.nextElementSibling
+
+        ans.style.display = ans.style.display === "block" ? "none" : "block"
+
+    }
+
+})
